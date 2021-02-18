4234945a13a4e638c13d36c3f32a4b7c5c3d6486
Issac Trotts <issac.trotts@gmail.com>
2021-02-17 20:50:54 -0700

    add live_view support

--- lib/delbug_web.ex
+++ lib/delbug_web.ex
@@ -24,6 +24,7 @@ defmodule DelbugWeb do
       import Plug.Conn
       import DelbugWeb.Gettext
       alias DelbugWeb.Router.Helpers, as: Routes
+      import Phoenix.LiveView.Controller
     end
   end
 
@@ -39,6 +40,24 @@ defmodule DelbugWeb do
 
       # Include shared imports and aliases for views
       unquote(view_helpers())
+      import Phoenix.LiveView.Helpers
+    end
+  end
+
+  def live_view do
+    quote do
+      use Phoenix.LiveView,
+        layout: {SpeakeasyWeb.LayoutView, "live.html"}
+
+      unquote(view_helpers())
+    end
+  end
+
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
+
+      unquote(view_helpers())
     end
   end
 
@@ -48,6 +67,7 @@ defmodule DelbugWeb do
 
       import Plug.Conn
       import Phoenix.Controller
+      import Phoenix.LiveView.Router
     end
   end
 
@@ -62,6 +82,9 @@ defmodule DelbugWeb do
     quote do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
+
+      # Import LiveView helpers (live_render, live_component, live_patch, etc)
+      import Phoenix.LiveView.Helpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
